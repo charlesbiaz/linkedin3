9b7e5b59223af01f3b0081191c26cae1e4ca1867
Charles Biaz <charles.biaz@gmail.com>
2022-07-13 12:05:47 +0200

    Initial Commit

---
+++ linkedin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  linkedin
+//
+//  Created by Charles Biaz on 13/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ linkedin/linkedinApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinApp.swift
+//  linkedin
+//
+//  Created by Charles Biaz on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct linkedinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
